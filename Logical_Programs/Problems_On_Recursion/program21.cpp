789837e0b8723c0ae7b6c30f4ecc7ec5be47f3fc
Tejas Sutar <tejassutar9001@gmail.com>
2026-04-15 23:51:01 +0530

    Function to find factorial of number using recursion and importance of pre and post decrement

---
+++ Logical_Programs/Problems_On_Recursion/program21.cpp
@@ -0,0 +1,29 @@
+// Input : 5
+// Output : 5*4*3*2*1 = 120
+
+#include<iostream>
+using namespace std;
+
+int Factorial(int iNo)
+{
+    int iFact = 1;
+
+    while(iNo >= 1)
+    {
+        iFact = iFact * iNo;
+        iNo--;
+    }
+
+    return iFact;
+}
+
+int main()
+{
+    int iRet = 0;
+
+    iRet = Factorial(5);
+
+    cout << "Factorial is : " << iRet << "\n";
+
+    return 0;
+}
