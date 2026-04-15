70de5b5965c31baf1def0eff91efd989ea392059
Tejas Sutar <tejassutar9001@gmail.com>
2026-04-15 23:53:14 +0530

    Problems on Digit,Numbers, N Numbers , String , Linkedlist using recursion

---
+++ Logical_Programs/Problems_On_Recursion/program26.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    iCnt = 1;
+
+    while(iCnt <= iNo)
+    {
+        cout << iCnt << "\t";
+        iCnt++;
+    }
+
+    cout << "\n";
+}
+
+int main()
+{
+    int iValue = 0;
+
+    cout << "Enter the nnumber : \n";
+    cin >> iValue;
+
+    Display(iValue);
+
+    return 0;
+}
